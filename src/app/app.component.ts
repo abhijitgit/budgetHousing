7665b8eec409ae7dbb9d049f4d41242266050410
Abhijit Moholkar <officemail.abhijit@gmail.com>
2024-04-17 09:25:58 +0930

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'homeListing';
---
+++ src/app/property/property-list/property-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'property-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './property-list.component.html',
+  styleUrl: './property-list.component.css',
+})
+export class PropertyListComponent {}
